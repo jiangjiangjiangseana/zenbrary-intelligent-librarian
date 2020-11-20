97dfc009194efa9b5b348c92ca2b3cc0ae05b7b2
jaclyn <jaing885@gmail.com>
2020-11-20 18:44:45 +0800

    adjust margin

--- ZenboDialogSample/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
+++ ZenboDialogSample/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
@@ -86,23 +86,23 @@ public final class R {
         public static final int async = 0x7f070021;
         public static final int blocking = 0x7f070026;
         public static final int chronometer = 0x7f070036;
-        public static final int forever = 0x7f070051;
-        public static final int icon = 0x7f07005b;
-        public static final int icon_group = 0x7f07005c;
-        public static final int info = 0x7f07006a;
-        public static final int italic = 0x7f07006c;
-        public static final int line1 = 0x7f07006d;
-        public static final int line3 = 0x7f07006e;
-        public static final int normal = 0x7f070080;
-        public static final int notification_background = 0x7f070081;
-        public static final int notification_main_column = 0x7f070082;
-        public static final int notification_main_column_container = 0x7f070083;
-        public static final int right_icon = 0x7f070091;
-        public static final int right_side = 0x7f070092;
-        public static final int text = 0x7f0700b2;
-        public static final int text2 = 0x7f0700b3;
-        public static final int time = 0x7f0700c1;
-        public static final int title = 0x7f0700c2;
+        public static final int forever = 0x7f070052;
+        public static final int icon = 0x7f07005c;
+        public static final int icon_group = 0x7f07005d;
+        public static final int info = 0x7f07006b;
+        public static final int italic = 0x7f07006d;
+        public static final int line1 = 0x7f07006e;
+        public static final int line3 = 0x7f07006f;
+        public static final int normal = 0x7f070081;
+        public static final int notification_background = 0x7f070082;
+        public static final int notification_main_column = 0x7f070083;
+        public static final int notification_main_column_container = 0x7f070084;
+        public static final int right_icon = 0x7f070092;
+        public static final int right_side = 0x7f070093;
+        public static final int text = 0x7f0700b3;
+        public static final int text2 = 0x7f0700b4;
+        public static final int time = 0x7f0700c2;
+        public static final int title = 0x7f0700c3;
     }
     public static final class integer {
         private integer() {}
